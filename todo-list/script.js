c737e023cb84024f74ef12ae00ed03b771ba2b18
jtornert <joakim.tornert@outlook.com>
2025-01-04 15:13:37 +0100

    CSP safe and type safe todo list persisted in localStorage

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,57 +1,74 @@
-function id() {
-    return "i" + Math.random().toString(36).slice(2);
-}
+/**
+ * @typedef {object} Task
+ * @property {string} id
+ * @property {string} title
+ * @property {boolean} completed
+ */
+
+var ui = {
+    id() {
+        return "i" + Math.random().toString(36).slice(2);
+    },
+};
 
 document.addEventListener("alpine:init", () => {
-    new Sortable(document.querySelector(".tasklist"), {
-        handle: "span.icon",
-        scroll: true,
-    });
-
-    Alpine.store("tasks", {
-        list: [],
-        init() {
-            const tasks = localStorage.getItem("tasks");
-            if (tasks) {
-                this.list = JSON.parse(tasks);
-            }
+    Alpine.data("app", () => ({
+        /** @type {Task[]} */
+        list: Alpine.$persist([]).as("tasks"),
+        classes: {
+            task() {
+                const id = this.$el.dataset.taskId;
+                const task = this.list.find((t) => t.id === id);
+                return task.completed ? { completed: true } : null;
+            },
         },
-        add(input) {
-            if (input.value.trim() === "") {
+        end(e) {
+            let newList = JSON.parse(localStorage.getItem("tasks")).toSpliced(
+                e.oldIndex,
+                1
+            );
+            newList.splice(e.newIndex, 0, { ...this.list[e.oldIndex] });
+            localStorage.setItem("tasks", JSON.stringify(newList));
+        },
+        add(e) {
+            const formData = new FormData(e.target);
+            const title = formData.get("title").trim();
+            if (title === "") {
                 return;
             }
             const task = {
-                id: id(),
-                title: input.value,
+                id: ui.id(),
+                title,
                 completed: false,
             };
             this.list.push(task);
-            this.save();
-            input.value = "";
-            return task;
+            e.target.querySelector("input[name=title]").value = "";
         },
-        remove(id) {
+        remove(e) {
+            const id = e.target.closest("[data-task-id]").dataset.taskId;
             this.list = this.list.filter((t) => t.id != id);
-            this.save();
         },
-        update(task, update, input) {
-            if (update.title.trim() === "") {
-                input.value = task.title.trim();
+        update(e) {
+            const id = e.target.closest("[data-task-id]").dataset.taskId;
+            const task = this.list.find((t) => t.id === id);
+
+            const title = e.target.value.trim();
+
+            if (title === "") {
+                e.target.value = task.title.trim();
                 return;
             }
+
             this.list = this.list.map((t) =>
-                t.id === task.id ? { ...t, ...update } : t
+                t.id === task.id ? { ...t, title } : t
             );
-            this.save();
         },
-        toggle(id) {
+        toggle(e) {
+            const id = e.target.closest("[data-task-id]").dataset.taskId;
+
             this.list = this.list.map((t) =>
                 t.id === id ? { ...t, completed: !t.completed } : t
             );
-            this.save();
         },
-        save() {
-            localStorage.setItem("tasks", JSON.stringify(this.list));
-        },
-    });
+    }));
 });
